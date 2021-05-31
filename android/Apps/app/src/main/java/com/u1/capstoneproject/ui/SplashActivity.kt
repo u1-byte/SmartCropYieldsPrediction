5a353dec026302116aea8b2d782b4958b175d63e
Yuma Gunawan <amazingyuma0@gmail.com>
2021-05-31 22:56:47 +0800

    add splash

--- android/Apps/app/src/main/java/com/u1/capstoneproject/ui/SplashActivity.kt
+++ android/Apps/app/src/main/java/com/u1/capstoneproject/ui/SplashActivity.kt
@@ -1,11 +1,12 @@
-package com.u1.capstoneproject
+package com.u1.capstoneproject.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.u1.capstoneproject.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
