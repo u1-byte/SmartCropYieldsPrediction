ee5047c6ab90e05dfa9526346e39631b203fa470
Yuma Gunawan <amazingyuma0@gmail.com>
2021-06-01 16:31:48 +0800

    setup onboarding and add about

---
+++ android/Apps/app/src/main/java/com/u1/capstoneproject/ui/about/AboutActivity.kt
@@ -0,0 +1,12 @@
+package com.u1.capstoneproject.ui.about
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.u1.capstoneproject.R
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
--- android/Apps/app/src/main/java/com/u1/capstoneproject/ui/splash/OnBoardingActivity.kt
+++ android/Apps/app/src/main/java/com/u1/capstoneproject/ui/splash/OnBoardingActivity.kt
@@ -1,11 +1,23 @@
 package com.u1.capstoneproject.ui.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.u1.capstoneproject.R
+import com.u1.capstoneproject.databinding.ActivityOnBoardingBinding
+import com.u1.capstoneproject.ui.home.MainActivity
 
 class OnBoardingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOnBoardingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_on_boarding)
+        setContentView(binding.root)
+
+        binding.btnNext.setOnClickListener {
+            startActivity(Intent(this@OnBoardingActivity, MainActivity::class.java))
+            finish()
+        }
     }
 }
