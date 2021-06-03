005205c2b769db94f18d95cc7464292fcac84e4f
Yuma Gunawan <amazingyuma0@gmail.com>
2021-06-03 13:32:38 +0800

    edit location activity and delete design link

--- android/Apps/app/src/main/java/com/u1/capstoneproject/ui/home/MainViewModel.kt
+++ android/Apps/app/src/main/java/com/u1/capstoneproject/ui/home/MainViewModel.kt
@@ -12,43 +12,4 @@ import retrofit2.Response
 
 class MainViewModel: ViewModel() {
 
-    var result = MutableLiveData<ResponseData>()
-
-    private val apiInstance = ApiHelper.instance
-
-    fun calculate(
-            param1: String,
-            param2: String,
-            param3: String,
-            param4: String,
-            param5: String,
-            param6: String,
-            param7: String,
-            param8: String,
-            param9: String,
-            param10: String,
-            param11: String,
-            param12: String,
-            param13: String,
-            param14: String
-    ){
-        apiInstance.getResult(
-                param1.toDouble(), param2.toDouble(), param3.toDouble(), param4.toDouble(), param5.toDouble(), param6.toDouble(), param7.toDouble(),
-                param8.toDouble(), param9.toDouble(), param10.toDouble(), param11.toDouble(), param12.toDouble(), param13.toDouble(), param14.toDouble()
-        ).enqueue(object :Callback<ResponseData>{
-            override fun onResponse(call: Call<ResponseData>, response: Response<ResponseData>) {
-                if (response.isSuccessful){
-                    result.postValue(response.body())
-                }
-            }
-
-            override fun onFailure(call: Call<ResponseData>, t: Throwable) {
-                Log.e("Fail", t.message.toString())
-            }
-        })
-    }
-
-    fun getResult(): LiveData<ResponseData>{
-        return result
-    }
 }
--- android/Apps/app/src/main/java/com/u1/capstoneproject/ui/setup_location/LocationSetupActivity.kt
+++ android/Apps/app/src/main/java/com/u1/capstoneproject/ui/setup_location/LocationSetupActivity.kt
@@ -3,10 +3,15 @@ package com.u1.capstoneproject.ui.setup_location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.u1.capstoneproject.R
+import com.u1.capstoneproject.databinding.ActivityLocationSetupBinding
 
 class LocationSetupActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLocationSetupBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_location_setup)
+        binding = ActivityLocationSetupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
