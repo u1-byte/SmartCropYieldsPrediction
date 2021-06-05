9c39335bda4d5b35487afee740f8697cff55513f
anggarapp <anggaraperpe@gmail.com>
2021-06-06 06:56:52 +0700

    change model location to local

--- cloud/backend/predict.py
+++ cloud/backend/predict.py
@@ -43,9 +43,14 @@ def predict(input_fd,output_fd,get_input,model_location):
     print(input_data)
     input = input_data.reshape((input_data.shape[0], 1, 1, 1, 12))
 
-    with FS.open(model_location, 'rb') as model_file:
-        model_gcs = h5py.File(model_file, 'r')
-        model = keras.models.load_model(model_gcs)
+    # Try Offline Model
+    model_gcs = h5py.File(model_location, 'r')
+    model = keras.models.load_model(model_gcs)
+
+    ## Online Model high response time
+    # with FS.open(model_location, 'rb') as model_file:
+    #     model_gcs = h5py.File(model_file, 'r')
+    #     model = keras.models.load_model(model_gcs)
     
     raw_prediction = model.predict(input)
     scaled_prediction = output_scaler.inverse_transform(raw_prediction)
@@ -60,7 +65,7 @@ def pred_temp():
     output_fit_data = [[26.49, 26.49, 26.49, 26.49],
                        [30.25, 30.25, 30.25, 30.25]]
     
-    model_location = 'gs://rice_price_dev/ml_models/temp_model.h5'
+    model_location = 'models/temp_model.h5'
 
     prediction = predict(input_fit_data,output_fit_data,query.get_temp(),model_location)
     return prediction
@@ -72,7 +77,7 @@ def pred_hum():
     output_fit_data = [[67.0, 67.0, 67.0, 67.0],
                        [85.45, 85.45, 85.45, 85.45]]
 
-    model_location = 'gs://rice_price_dev/ml_models/hum_model.h5'
+    model_location = 'models/hum_model.h5'
 
     prediction = predict(input_fit_data,output_fit_data,query.get_hum(),model_location)
     return prediction
@@ -84,7 +89,7 @@ def pred_rain():
     output_fit_data = [[0, 0, 0, 0],
                        [21.84, 21.84, 21.84, 21.84]]
     
-    model_location = 'gs://rice_price_dev/ml_models/rain_model.h5'
+    model_location = 'models/rain_model.h5'
 
     prediction = predict(input_fit_data,output_fit_data,query.get_rain(),model_location)
     return prediction
@@ -96,7 +101,7 @@ def pred_shine():
     output_fit_data = [[3.27, 3.27, 3.27, 3.27],
                        [9.47, 9.47, 9.47, 9.47]]
     
-    model_location = 'gs://rice_price_dev/ml_models/shine_model.h5'
+    model_location = 'models/shine_model.h5'
 
     prediction = predict(input_fit_data,output_fit_data,query.get_shine(),model_location)
     return prediction
